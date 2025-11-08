d7205973bc3ab4703f4ad161849ce096c5b2e9de
pooreumjung <pooreumjung02@naver.com>
2025-11-08 16:54:42 +0900

    feat: OpenAI Clinet 추가

--- app/core/config.py
+++ app/core/config.py
@@ -12,3 +12,6 @@ DB_NAME = os.getenv("DB_NAME")
 
 # Construct the database URL
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+
+# OpenAI API configuration
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
